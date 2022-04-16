8ab5ab41380691979d263bc08fced483a0cda806
cloud-automation-marcel <80656229+cloud-automation-marcel@users.noreply.github.com>
2022-04-16 13:33:47 -0500

    Add list files

--- starz.py
+++ starz.py
@@ -1,4 +1,14 @@
+import os
+
+
 loop_count = 10
 
 for row in range(loop_count):
     print("TESTING!!!!", row)
+
+
+
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    print(f)
